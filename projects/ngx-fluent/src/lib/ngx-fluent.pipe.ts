a14fe2920f47950a2f27294a7c9d29a2c93fd006
Cerino Ligutom III <zeferinix@gmail.com>
2023-03-09 04:02:17 +0800

    feat: pipe should be reactive

--- projects/ngx-fluent/src/lib/ngx-fluent.pipe.ts
+++ projects/ngx-fluent/src/lib/ngx-fluent.pipe.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NgxFluentService } from './ngx-fluent.service';
+import { isEqual } from 'lodash-es';
 
 @Pipe({
   name: 'fluent',
@@ -11,14 +12,24 @@ export class NgxFluentPipe implements PipeTransform {
 
   localeChanges: Observable<string | null> | null = null;
 
+  previousArgs?: any;
+
   constructor(private readonly fluentService: NgxFluentService) {}
 
-  transform(key: string, args?: any): string {
+  transform(key: string, args?: any) {
     if (!this.localeChanges) {
       this.localeChanges = this.fluentService.localeChanges;
 
       this.localeChanges.subscribe(async () => {
-        this.value = await this.fluentService.translate(key, args);
+        this.value = await this.fluentService.translate(key, this.previousArgs ?? args);
+      });
+    }
+
+    // Could probably be optimized to only update when the args change?
+    if (!isEqual(args, this.previousArgs)) {
+      this.previousArgs = args;
+      this.fluentService.translate(key, args).then((value) => {
+        this.value = value;
       });
     }
 
