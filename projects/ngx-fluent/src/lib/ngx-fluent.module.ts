f76b6f7be42df8bb8b8dc060098dda2f7ca09b13
Cerino Ligutom III <zeferinix@gmail.com>
2024-06-02 09:59:08 +0800

    chore: fix lint issues

--- projects/ngx-fluent/src/lib/ngx-fluent.module.ts
+++ projects/ngx-fluent/src/lib/ngx-fluent.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { NgxFluentPipe } from './ngx-fluent.pipe';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({ declarations: [NgxFluentPipe],
-    exports: [NgxFluentPipe], imports: [], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [NgxFluentPipe],
+  exports: [NgxFluentPipe],
+  imports: [],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
 export class NgxFluentModule {}
